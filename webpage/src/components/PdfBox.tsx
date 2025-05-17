a300f40bef1126197c6a8aaa750c8d954d6501b2
Aditya Verma <aditya1310v@gmail.com>
2025-05-17 11:54:14 +0530

    Enable Download Pdf for phone

--- webpage/src/components/PdfBox.tsx
+++ webpage/src/components/PdfBox.tsx
@@ -5,10 +5,20 @@ function PdfBox({
   pdfUrl: string | null;
   defaultPdfUrl: string;
 }) {
+  const handlePdfDownload = () => {
+    const link = document.createElement('a');
+    link.href = pdfUrl || defaultPdfUrl;
+    link.download = 'ineedresume.pdf';
+    link.click();
+    document.body.removeChild(link);
+  }
   return (
+   
+    <>
     <div>
       {pdfUrl && (
         <div className="pdf-viewer-z-0 container lg:fixed lg:top-19 lg:right-0 lg:w-1/2 sm:w-full">
+          <button onClick={handlePdfDownload}>Download PDF</button>
           <iframe
             src={pdfUrl + "#zoom=90%"}
             className="w-full h-screen rounded-xl border border-white/10"
@@ -17,6 +27,7 @@ function PdfBox({
       )}
       {!pdfUrl && (
         <div className="pdf-viewer-container lg:fixed lg:top-19 lg:right-0 lg:w-1/2 sm:w-full">
+          <button onClick={handlePdfDownload}>Download PDF</button>
           <iframe
             src={defaultPdfUrl + "#zoom=90%"}
             className="w-full h-screen rounded-xl border border-white/10"
@@ -24,6 +35,7 @@ function PdfBox({
         </div>
       )}
     </div>
+    </>
   );
 }
 
